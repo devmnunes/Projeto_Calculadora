578862bdcce981b51feb4e30bbb1a86854224dbe
Matheus Nunes <matheus_gustavo97@hotmail.com>
2025-04-26 15:13:50 -0300

    commit

--- calculadora/lib/main.dart
+++ calculadora/lib/main.dart
@@ -17,63 +17,123 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Center(
-            child: Text('Calculadora')
-            ),
+          title: Center(child: Text('Calculadora')),
           backgroundColor: Colors.blue,
         ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             Text('Coluna 1'),
-            
+
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Text('AC', style: TextStyle(fontSize: 36),),
-                Text(''),
-                Text(''),
-                Text('<X', style: TextStyle(fontSize: 36),),
+                Container(
+                  child: Center(
+                    child: Text('AC', style: TextStyle(fontSize: 30)),
+                  ),
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(33),
+                  ),
+                ),
+
+                Container(
+                  child: Center(
+                    child: Text('<X', style: TextStyle(fontSize: 30)),
+                  ),
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(35),
+                  ),
+                ),
               ],
             ),
 
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Text('7', style: TextStyle(fontSize: 36),),
-                Text('8', style: TextStyle(fontSize: 36),),
-                Text('9', style: TextStyle(fontSize: 36),),
-                Text('/', style: TextStyle(fontSize: 36),),
+                Container(
+                  child: Center(
+                    child: Text('7', style: TextStyle(fontSize: 30)),
+                  ),
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(33),
+                  ),
+                ),
+
+                Container(
+                  child: Center(
+                    child: Text('8', style: TextStyle(fontSize: 30)),
+                  ),
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(33),
+                  ),
+                ),
+
+                Container(
+                  child: Center(
+                    child: Text('9', style: TextStyle(fontSize: 30)),
+                  ),
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(33),
+                  ),
+                ),
+
+                Container(
+                  child: Center(
+                    child: Text('/', style: TextStyle(fontSize: 30)),
+                  ),
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 248, 132, 0),
+                    borderRadius: BorderRadius.circular(33),
+                  ),
+                ),
               ],
             ),
-  
+
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Text('4', style: TextStyle(fontSize: 36),),
-                Text('5', style: TextStyle(fontSize: 36),),
-                Text('6', style: TextStyle(fontSize: 36),),
-                Text('x', style: TextStyle(fontSize: 36),),
+                Text('4', style: TextStyle(fontSize: 36)),
+                Text('5', style: TextStyle(fontSize: 36)),
+                Text('6', style: TextStyle(fontSize: 36)),
+                Text('x', style: TextStyle(fontSize: 36)),
               ],
             ),
-            
+
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Text('1', style: TextStyle(fontSize: 36),),
-                Text('2', style: TextStyle(fontSize: 36),),
-                Text('3', style: TextStyle(fontSize: 36),),
-                Text('-', style: TextStyle(fontSize: 36),),
+                Text('1', style: TextStyle(fontSize: 36)),
+                Text('2', style: TextStyle(fontSize: 36)),
+                Text('3', style: TextStyle(fontSize: 36)),
+                Text('-', style: TextStyle(fontSize: 36)),
               ],
             ),
-            
+
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Text('0', style: TextStyle(fontSize: 36),),
-                Text(',', style: TextStyle(fontSize: 36),),
-                Text('=', style: TextStyle(fontSize: 36),),
-                Text('+', style: TextStyle(fontSize: 36),),
+                Text('0', style: TextStyle(fontSize: 36)),
+                Text(',', style: TextStyle(fontSize: 36)),
+                Text('=', style: TextStyle(fontSize: 36)),
+                Text('+', style: TextStyle(fontSize: 36)),
               ],
             ),
             Text('Coluna 6'),
