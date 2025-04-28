bc5ef7b88a9ef6762dd418961cf40615f4b137bf
Matheus Nunes <143001745+devmnunes@users.noreply.github.com>
2025-04-28 17:20:01 -0300

    commit

--- calculadora/lib/main.dart
+++ calculadora/lib/main.dart
@@ -12,7 +12,45 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  String numero = 'Número';
   int calculo = 0;
+
+  void calcular( String tecla) {
+
+  switch(tecla) {
+
+    case 'AC':
+    setState(() {
+       numero = '0';
+    });
+      break;
+
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+    case '/':
+    case 'x':
+    case '+':
+    case '-':
+    case ',':
+    setState(() {
+       numero += tecla;
+    });
+      break;
+
+
+      default:
+        numero += tecla;
+      break;
+  }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,7 +72,12 @@ class _MyAppState extends State<MyApp> {
                 border: Border.all(color: Colors.black, width: 3),
               ),
               child: Center(
-                child: Text('Coluna 1'),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text( numero, style: TextStyle(fontSize: 72),),
+                  ],
+                ),
                 ),
               ),
 
@@ -42,14 +85,18 @@ class _MyAppState extends State<MyApp> {
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 Container(
-                  width: 55,
-                  height: 55,
+                  width: 75,
+                  height: 60,
                   decoration: BoxDecoration(
                     color: Colors.grey,
                     borderRadius: BorderRadius.circular(33),
                   ),
                   child: Center(
-                    child: Text('AC', style: TextStyle(fontSize: 30)),
+                    child: GestureDetector(
+                      onTap:() {
+                        calcular('AC');
+                      },
+                      child: Text('AC', style: TextStyle(fontSize: 48))),
                   ),
                 ),
 
@@ -61,7 +108,11 @@ class _MyAppState extends State<MyApp> {
                     borderRadius: BorderRadius.circular(35),
                   ),
                   child: Center(
-                    child: Image.asset('assets/image/backspace.png'),
+                    child: GestureDetector(
+                      onTap: () {
+                        calcular('assets/image/backspace.png');
+                      },
+                      child: Image.asset('assets/image/backspace.png')),
                   ),
                 ),
               ],
@@ -78,7 +129,11 @@ class _MyAppState extends State<MyApp> {
                     borderRadius: BorderRadius.circular(33),
                   ),
                   child: Center(
-                    child: Text('7', style: TextStyle(fontSize: 30)),
+                    child: GestureDetector(
+                      onTap: () { 
+                        calcular('7');
+                        },
+                      child: Text('7', style: TextStyle(fontSize: 48))),
                   ),
                 ),
 
@@ -90,7 +145,11 @@ class _MyAppState extends State<MyApp> {
                     borderRadius: BorderRadius.circular(33),
                   ),
                   child: Center(
-                    child: Text('8', style: TextStyle(fontSize: 30)),
+                    child: GestureDetector(
+                      onTap: () {
+                        calcular('8');
+                      },
+                      child: Text('8', style: TextStyle(fontSize: 48))),
                   ),
                 ),
 
@@ -102,7 +161,11 @@ class _MyAppState extends State<MyApp> {
                     borderRadius: BorderRadius.circular(33),
                   ),
                   child: Center(
-                    child: Text('9', style: TextStyle(fontSize: 30)),
+                    child: GestureDetector(
+                      onTap: () {
+                        calcular('9');
+                      },
+                      child: Text('9', style: TextStyle(fontSize: 48))),
                   ),
                 ),
 
@@ -114,7 +177,11 @@ class _MyAppState extends State<MyApp> {
                     borderRadius: BorderRadius.circular(33),
                   ),
                   child: Center(
-                    child: Text('/', style: TextStyle(fontSize: 30)),
+                    child: GestureDetector(
+                      onTap: () {
+                        calcular('/');
+                      },
+                      child: Text('/', style: TextStyle(fontSize: 48))),
                   ),
                 ),
               ],
@@ -131,7 +198,11 @@ class _MyAppState extends State<MyApp> {
                     borderRadius: BorderRadius.circular(33),
                   ),
                   child: Center(
-                    child: Text('4', style: TextStyle(fontSize: 36)),
+                    child: GestureDetector(
+                      onTap: () {
+                        calcular('4');
+                      },
+                      child: Text('4', style: TextStyle(fontSize: 48))),
                   ),
                 ),
 
@@ -143,7 +214,11 @@ class _MyAppState extends State<MyApp> {
                     borderRadius: BorderRadius.circular(33),
                   ),
                   child: Center(
-                    child: Text('5', style: TextStyle(fontSize: 36)),
+                    child: GestureDetector(
+                      onTap: () {
+                        calcular('5');
+                      },
+                      child: Text('5', style: TextStyle(fontSize: 48))),
                   ),
                 ),
 
@@ -155,7 +230,11 @@ class _MyAppState extends State<MyApp> {
                     borderRadius: BorderRadius.circular(33),
                   ),
                   child: Center(
-                    child: Text('6', style: TextStyle(fontSize: 36)),
+                    child: GestureDetector(
+                      onTap: () {
+                        calcular('6');
+                      },
+                      child: Text('6', style: TextStyle(fontSize: 48))),
                   ),
                 ),
 
@@ -167,7 +246,11 @@ class _MyAppState extends State<MyApp> {
                     borderRadius: BorderRadius.circular(33),
                   ),
                   child: Center(
-                    child: Text('x', style: TextStyle(fontSize: 30)),
+                    child: GestureDetector(
+                      onTap: () {
+                        calcular('x');
+                      },
+                      child: Text('x', style: TextStyle(fontSize: 48))),
                   ),
                 ),
               ],
@@ -184,7 +267,11 @@ class _MyAppState extends State<MyApp> {
                     borderRadius: BorderRadius.circular(33),
                   ),
                   child: Center(
-                    child: Text('1', style: TextStyle(fontSize: 36)),
+                    child: GestureDetector(
+                      onTap: () {
+                        calcular('1');
+                      },
+                      child: Text('1', style: TextStyle(fontSize: 48))),
                   ),
                 ),
 
@@ -196,7 +283,11 @@ class _MyAppState extends State<MyApp> {
                     borderRadius: BorderRadius.circular(33),
                   ),
                   child: Center(
-                    child: Text('2', style: TextStyle(fontSize: 36)),
+                    child: GestureDetector(
+                      onTap: () {
+                        calcular('2');
+                      },
+                      child: Text('2', style: TextStyle(fontSize: 48))),
                   ),
                 ),
 
@@ -208,7 +299,11 @@ class _MyAppState extends State<MyApp> {
                     borderRadius: BorderRadius.circular(33),
                   ),
                   child: Center(
-                    child: Text('3', style: TextStyle(fontSize: 36)),
+                    child: GestureDetector(
+                      onTap: () {
+                        calcular('3');
+                      },
+                      child: Text('3', style: TextStyle(fontSize: 48))),
                   ),
                 ),
 
@@ -220,7 +315,11 @@ class _MyAppState extends State<MyApp> {
                     borderRadius: BorderRadius.circular(33),
                   ),
                   child: Center(
-                    child: Text('-', style: TextStyle(fontSize: 30)),
+                    child: GestureDetector(
+                      onTap: () {
+                        calcular('-');
+                      },
+                      child: Text('-', style: TextStyle(fontSize: 48))),
                   ),
                 ),
               ],
@@ -237,7 +336,27 @@ class _MyAppState extends State<MyApp> {
                     borderRadius: BorderRadius.circular(33),
                   ),
                   child: Center(
-                    child: Text('0', style: TextStyle(fontSize: 36)),
+                    child: GestureDetector(
+                      onTap: () {
+                        calcular('0');
+                      },
+                      child: Text('0', style: TextStyle(fontSize: 48))),
+                  ),
+                ),
+
+                Container(
+                  width: 55,
+                  height: 55,
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(48),
+                  ),
+                  child: Center(
+                    child: GestureDetector(
+                      onTap: () {
+                        calcular(',');
+                      },
+                      child: Text(',', style: TextStyle(fontSize: 48))),
                   ),
                 ),
 
@@ -249,19 +368,11 @@ class _MyAppState extends State<MyApp> {
                     borderRadius: BorderRadius.circular(33),
                   ),
                   child: Center(
-                    child: Text(',', style: TextStyle(fontSize: 36)),
-                  ),
-                ),
-
-                Container(
-                  width: 55,
-                  height: 55,
-                  decoration: BoxDecoration(
-                    color: Colors.grey,
-                    borderRadius: BorderRadius.circular(33),
-                  ),
-                  child: Center(
-                    child: Text('=', style: TextStyle(fontSize: 36)),
+                    child: GestureDetector(
+                      onTap: () {
+                        calcular('=');
+                      },
+                      child: Text('=', style: TextStyle(fontSize: 48))),
                   ),
                 ),
 
@@ -274,7 +385,11 @@ class _MyAppState extends State<MyApp> {
                     borderRadius: BorderRadius.circular(33),
                   ),
                   child: Center(
-                    child: Text('+', style: TextStyle(fontSize: 30)),
+                    child: GestureDetector(
+                      onTap: () {
+                        calcular('+');
+                      },
+                      child: Text('+', style: TextStyle(fontSize: 48))),
                   ),
                 ),
               ],
