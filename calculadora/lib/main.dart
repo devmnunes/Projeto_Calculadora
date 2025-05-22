cc2a00d7b2ece7fc8d68956a1b6cddbd5ab21aea
Matheus Nunes <143001745+devmnunes@users.noreply.github.com>
2025-05-22 13:23:00 -0300

    commit

--- calculadora/lib/main.dart
+++ calculadora/lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:calculadora/modules/home.dart';
+
+import 'package:calculadora/app/modules/home.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
