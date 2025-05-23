c2bfc25d8596ba8ed5ec07e0ea5e8857cc3dc1e4
Matheus Nunes <matheus_gustavo97@hotmail.com>
2025-05-22 21:10:46 -0300

    commit

--- calculadora/lib/app/modules/home.dart
+++ calculadora/lib/app/modules/home.dart
@@ -45,7 +45,6 @@ class _MyAppState extends State<MyApp> {
         });
         break;
 
-     
       case '+':
       case '-':
       case 'x':
@@ -60,29 +59,28 @@ class _MyAppState extends State<MyApp> {
       case '=':
         double resultado = 0.0;
 
-        if (operacao == '/'){
+        if (operacao == '/') {
           if (double.parse(numero) * 1 == 0) {
             print('ERRO: divisão por zero');
             return;
           }
         }
 
-        if (operacao == '+'){
-            resultado =  primeiroNum + double.parse(numero);
+        if (operacao == '+') {
+          resultado = primeiroNum + double.parse(numero);
         }
 
-        if (operacao == '-'){
+        if (operacao == '-') {
           resultado = primeiroNum - double.parse(numero);
         }
 
-         if (operacao == 'x'){
+        if (operacao == 'x') {
           resultado = primeiroNum * double.parse(numero);
         }
 
-         if (operacao == '/'){
+        if (operacao == '/') {
           resultado = primeiroNum / double.parse(numero);
         }
-
 
         setState(() {
           numero = resultado.toString();
@@ -97,13 +95,12 @@ class _MyAppState extends State<MyApp> {
         break;
 
       case 'assets/image/backspace.png':
+        setState(() {
+          if (numero.length > 0) {
+            numero = numero.substring(0, numero.length - 1);
+          }
+        });
 
-      setState(() {
-        if (numero.length > 0) {
-          numero = numero.substring(0, numero.length -1);
-        } 
-      });
-        
         break;
 
       default:
@@ -124,105 +121,201 @@ class _MyAppState extends State<MyApp> {
         body: Column(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-  Container(
-    width: double.infinity,
-    height: 100,
-    decoration: BoxDecoration(
-      color: const Color(0xF6F6F6F6),
-      border: Border.all(color: Colors.black, width: 4),
-    ),
-    child: Center(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          Text(numero, style: TextStyle(fontSize: 72)),
-        ],
-      ),
-    ),
-  ),
-
-  Row(
-    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-    children: [
-
-      ButtomCustomNum('', text: 'AC', onPressed: (String) {calcular('AC');}),
-
-      Container(
-        width: 75,
-        height: 60,
-        decoration: BoxDecoration(
-          color: Colors.grey,
-          borderRadius: BorderRadius.circular(35),
-        ),
-        child: Center(
-          child: GestureDetector(
-            onTap: () {
-              calcular('assets/image/backspace.png');
-            },
-            child: Image.asset('assets/image/backspace.png'),
-          ),
-        ),
-      ),
-    ],
-  ),
-
+            Container(
+              width: double.infinity,
+              height: 100,
+              decoration: BoxDecoration(
+                color: const Color(0xF6F6F6F6),
+                border: Border.all(color: Colors.black, width: 4),
+              ),
+              child: Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [Text(numero, style: TextStyle(fontSize: 72))],
+                ),
+              ),
+            ),
 
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                ButtomCustomNum('', text: '7', onPressed: (String) {calcular('7');}),
+                ButtomCustomNum(
+                  '',
+                  text: 'AC',
+                  onPressed: (String) {
+                    calcular('AC');
+                  },
+                ),
 
-                ButtomCustomNum('', text: '8', onPressed: (String) {calcular('8');}),
+                Container(
+                  width: 75,
+                  height: 60,
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(35),
+                  ),
+                  child: Center(
+                    child: GestureDetector(
+                      onTap: () {
+                        calcular('assets/image/backspace.png');
+                      },
+                      child: Image.asset('assets/image/backspace.png'),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ButtomCustomNum(
+                  '',
+                  text: '7',
+                  onPressed: (String) {
+                    calcular('7');
+                  },
+                ),
 
-                ButtomCustomNum('', text: '9', onPressed: (String) {calcular('9');}),
+                ButtomCustomNum(
+                  '',
+                  text: '8',
+                  onPressed: (String) {
+                    calcular('8');
+                  },
+                ),
 
-                ButtonCustomOP( '', text: '/', onPressed: (String ) { calcular('/');  }),
+                ButtomCustomNum(
+                  '',
+                  text: '9',
+                  onPressed: (String) {
+                    calcular('9');
+                  },
+                ),
+
+                ButtonCustomOP(
+                  '',
+                  text: '/',
+                  onPressed: (String) {
+                    calcular('/');
+                  },
+                ),
               ],
             ),
 
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                ButtomCustomNum('', text: '4', onPressed: (String) {calcular('4');}),
+                ButtomCustomNum(
+                  '',
+                  text: '4',
+                  onPressed: (String) {
+                    calcular('4');
+                  },
+                ),
 
-                ButtomCustomNum('', text: '5', onPressed: (String) {calcular('5');}),
+                ButtomCustomNum(
+                  '',
+                  text: '5',
+                  onPressed: (String) {
+                    calcular('5');
+                  },
+                ),
 
-                ButtomCustomNum('', text: '6', onPressed: (String) {calcular('6');}),
+                ButtomCustomNum(
+                  '',
+                  text: '6',
+                  onPressed: (String) {
+                    calcular('6');
+                  },
+                ),
 
-                ButtonCustomOP( '', text: 'x', onPressed: (String ) { calcular('x');  }),
+                ButtonCustomOP(
+                  '',
+                  text: 'x',
+                  onPressed: (String) {
+                    calcular('x');
+                  },
+                ),
               ],
             ),
 
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                ButtomCustomNum('', text: '1', onPressed: (String) {calcular('1');}),
+                ButtomCustomNum(
+                  '',
+                  text: '1',
+                  onPressed: (String) {
+                    calcular('1');
+                  },
+                ),
 
-                ButtomCustomNum('', text: '2', onPressed: (String) {calcular('2');}),
+                ButtomCustomNum(
+                  '',
+                  text: '2',
+                  onPressed: (String) {
+                    calcular('2');
+                  },
+                ),
 
-                ButtomCustomNum('', text: '3', onPressed: (String) {calcular('3');}),
+                ButtomCustomNum(
+                  '',
+                  text: '3',
+                  onPressed: (String) {
+                    calcular('3');
+                  },
+                ),
 
-                ButtonCustomOP( '', text: '-', onPressed: (String ) { calcular('-');  }),
+                ButtonCustomOP(
+                  '',
+                  text: '-',
+                  onPressed: (String) {
+                    calcular('-');
+                  },
+                ),
               ],
             ),
 
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                ButtomCustomNum('', text: '0', onPressed: (String) {calcular('0');}),
+                ButtomCustomNum(
+                  '',
+                  text: '0',
+                  onPressed: (String) {
+                    calcular('0');
+                  },
+                ),
 
-                ButtomCustomNum('', text: ',', onPressed: (String) {calcular(',');}),
+                ButtomCustomNum(
+                  '',
+                  text: ',',
+                  onPressed: (String) {
+                    calcular(',');
+                  },
+                ),
 
-                ButtomCustomNum('', text: '=', onPressed: (String) {calcular('=');}),
+                ButtomCustomNum(
+                  '',
+                  text: '=',
+                  onPressed: (String) {
+                    calcular('=');
+                  },
+                ),
 
-                ButtonCustomOP( '', text: '+', onPressed: (String ) { calcular('+');  }),
+                ButtonCustomOP(
+                  '',
+                  text: '+',
+                  onPressed: (String) {
+                    calcular('+');
+                  },
+                ),
               ],
             ),
             Text('Criado por Matheus Nunes'),
           ],
         ),
       ),
-      );
+    );
   }
 }
-
